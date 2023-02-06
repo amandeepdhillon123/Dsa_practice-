9b00471b50859cfd0fe2ae5062a49f8e6cdf96fe
amandeepdhillon123 <amandeep24397@gmail.com>
2023-02-06 09:00:53 +0530

    pyramid

---
+++ pattern/pyramid.cpp
@@ -0,0 +1,31 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int value;
+    cout << " put the value :";
+    cin >> value;
+/*
+    for( int i=0; i<value; i++)
+    {
+        for( int j=0; j<i+1; j++)
+        {
+            cout << "* ";
+        }
+          cout << endl;
+    }
+
+    */
+
+    // reverse pyramid 
+
+     for( int i=value; i>0; i--)
+    {
+        for( int j=0; j<=i-1; j++)
+        {
+            cout << "* ";
+        }
+          cout << endl;
+    }
+  
+}
